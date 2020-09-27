9ccb04d7fdbce0a479341897951f2518c3d3e39e
SolymarN <solymar2192@gmail.com>
2020-09-26 19:37:49 -0500

    api instagram

---
+++ app/src/main/java/com/ivamadoka/mascotasapp/ConfigCuentaActivity.java
@@ -0,0 +1,32 @@
+package com.ivamadoka.mascotasapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ConfigCuentaActivity extends AppCompatActivity {
+
+    private Button btnGuardar;
+    private EditText edCuenta;
+    private Toolbar miActionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_cuenta);
+        miActionBar = findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        edCuenta = (EditText) findViewById(R.id.tilCuenta);
+        btnGuardar = (Button) findViewById(R.id.btnGuardar);
+
+    }
+}
